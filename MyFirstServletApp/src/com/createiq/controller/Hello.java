2f19ec10bd12d8e6b4fb5e4cf2ec65ab54c447fd
createiqs <createiqs@gmail.com>
2022-06-14 10:45:17 +0530

    my first servlet application develop code

---
+++ MyFirstServletApp/src/com/createiq/controller/Hello.java
@@ -0,0 +1,22 @@
+package com.createiq.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Hello extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		PrintWriter writer = resp.getWriter();
+		writer.println("<h1>Hello World!....</h1>");
+		writer.println("<p> Servlet is a technology used to create dynamic web applications</p>");
+		writer.close();
+	}
+
+}
